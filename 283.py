8fecebb6ec842c6649598621bc1e7fe6ef6d4df6
2428036768 <2428036768@qq.com>
2020-11-19 17:08:39 +0800

    移动零

---
+++ 283.py
@@ -0,0 +1,34 @@
+"""
+    简单：移动零
+    双指针
+    """
+
+
+def moveZeroes(nums):
+    left = 0
+    right = 1
+    while right < len(nums):  # 控制右指针
+        if nums[right]==0:
+            right+=1
+            continue
+        while left < right:
+            if nums[left] == 0:
+                nums[left],nums[right]=nums[right],nums[left]
+                break
+            left+=1
+        right+=1
+    return nums
+
+print(moveZeroes( [3,12]))
+
+
+#官方题解
+    # def moveZeroes(self, nums: List[int]) -> None:
+    #     n = len(nums)
+    #     left = right = 0
+    #     while right < n:
+    #         if nums[right] != 0:
+    #             nums[left], nums[right] = nums[right], nums[left]
+    #             left += 1
+    #         right += 1
+        
