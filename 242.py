97b73ebea43807ba305cd41dc400013f097dbd85
2428036768 <2428036768@qq.com>
2020-11-22 22:07:33 +0800

    有效字母异位

--- 242.py
+++ 242.py
@@ -1,13 +1,11 @@
 """
 有效字母异位
 简单
+复杂度：排序的时间复杂度为 O(nlogn)，比较两个字符串是否相等时间复杂度为 O(n)，因此总体时间复杂度为 O(nlogn+n)=O(nlogn)。
 """
 
 def isAnagram(s, t):
-    if sorted(s) == sorted(t):
-        return True
-    else:
-        return False
+    return sorted(s) == sorted(t)
 
 s = "rat"
 t = "car"
