efc95a7c20aaa77621903a49154213ae882c6068
2428036768@qq.com <2428036768@qq.com>
2020-12-08 21:45:05 +0800

    回溯+剪枝--抄的抄的--中等--要看要看

--- 842.py
+++ 842.py
@@ -1,34 +1,36 @@
 """
 回溯+剪枝
 抄的抄的
-
+将数组拆分成斐波那契序列
 中等
 """
-class Solution:
-    def splitIntoFibonacci(self, S: str) -> List[int]:
-        ans = list()
+def splitIntoFibonacci(S):
+    ans = list()
 
-        def backtrack(index: int):
-            if index == len(S):
-                return len(ans) >= 3
+    def backtrack(index: int):
+        if index == len(S):
+            return len(ans) >= 3
+        
+        curr = 0
+        for i in range(index, len(S)):
+            if i > index and S[index] == "0":
+                break
+            curr = curr * 10 + ord(S[i]) - ord("0")
+            if curr > 2**31 - 1:
+                break
             
-            curr = 0
-            for i in range(index, len(S)):
-                if i > index and S[index] == "0":
-                    break
-                curr = curr * 10 + ord(S[i]) - ord("0")
-                if curr > 2**31 - 1:
-                    break
-                
-                if len(ans) < 2 or curr == ans[-2] + ans[-1]:
-                    ans.append(curr)
-                    if backtrack(i + 1):
-                        return True
-                    ans.pop()
-                elif len(ans) > 2 and curr > ans[-2] + ans[-1]:
-                    break
-        
-            return False
-        
-        backtrack(0)
-        return ans
+            if len(ans) < 2 or curr == ans[-2] + ans[-1]:
+                ans.append(curr)
+                if backtrack(i + 1):
+                    return True
+                ans.pop()
+            elif len(ans) > 2 and curr > ans[-2] + ans[-1]:
+                break
+    
+        return False
+    
+    backtrack(0)
+    return ans
+
+S="123456579"
+print(splitIntoFibonacci(S))
